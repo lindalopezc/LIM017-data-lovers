9f23cd88e225c2b40e20a29de4c7340a34e61dc1
lindalopezc <lindalopezramos61@gmail.com>
2022-03-17 11:56:24 -0500

    Updates

--- data.js
+++ data.js
@@ -54,20 +54,33 @@ export const computeStats=(data,position)=>{
   for(let element of data){
       arrayTeam.push(element.team);
   }
-//Generamos un objeto con los nombres de los países y la cantidad de medallas. 
-let medalsByCountry ={};
+  //Generamos un objeto con los nombres de los países y la cantidad de medallas. 
+  let medalsByCountry ={};
   for(let element of arrayTeam){
       (medalsByCountry[element]) ? medalsByCountry[element] += 1 : medalsByCountry[element] = 1;
   }
-// Creamos un array con keys "name" y "valor" para guardar nombres de países y número de medallas.
-let array=[];
+  // Creamos un array con keys "name" y "valor" para guardar nombres de países y número de medallas.
+  let array=[];
   for(let propiedad in medalsByCountry){
     array.push({name:propiedad, valor: medalsByCountry[propiedad]})
   }
-//Ordenamos los países por número de medallas obtenidas de manera descendente, retornando un array con los 10 primeros.
-array.sort((a,b)=>{return b.valor - a.valor})
+  //Ordenamos los países por número de medallas obtenidas de manera descendente, retornando un array con los 10 primeros.
+  array.sort((a,b)=>{return b.valor - a.valor})
   for(let i=array.length; i>position;i--){
     array.pop(array[i]);
   }
-return array;
+  return array;
+}
+
+export const porcentGender=(data,total) =>{
+  
+  //Generamos un objeto para conocer la cantidad de mujeres y varones
+  let array =[{gender:'Femenino', valor:0},{gender:'Masculino', valor:0}];
+  for(let element of data){
+      (element.gender === 'F' || element.gender ==='🙋🏻‍♀️')? array[0].valor += 1: array[1].valor += 1;
+  }
+  //Convertimos los valores obtenidos de mujeres y varones en porcentaje:
+  array[0].valor=Math.round((array[0].valor/total)*100);
+  array[1].valor=Math.round((array[1].valor/total)*100);
+  return array;
 }
--- graphics.js
+++ graphics.js
@@ -1,2 +1,106 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
+// eslint-disable-next-line no-unused-vars
+import{dataForAthletes} from './main.js';
+import{computeStats,porcentGender} from './data.js';
 
+
+const myChart = document.getElementById("myChart");
+const graphicForGender=document.getElementById('graphic-gender');
+let teams = [];
+let value =[];
+for(let element of computeStats(dataForAthletes,10)){
+    teams.push(element.name);
+    value.push(element.valor);
+}
+
+const firstGraphic = new Chart(myChart, {
+  type: "bar",
+  data: {
+    labels: teams,
+    datasets: [
+      {
+        label: "Medallas obtenidas",
+        data: value,
+        backgroundColor: [
+            'rgb(154, 220, 255)',
+            'rgb(255, 248, 154)',
+            'rgb(255, 178, 166)',
+            'rgb(255, 138, 174)',
+            'rgb(212, 122, 232)',
+            'rgb(244, 190, 238)',
+            'rgb(168, 236, 231)',
+            'rgb(244, 190, 238)',
+            'rgb(216, 133, 163)',
+            'rgb(120, 151, 171)',
+            ],
+      },
+    ],
+  },
+  options: {
+    indexAxis: "y",
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    maintainAspectRatio:false,
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+      title:{
+        display:true,
+        align:'center',
+        position:'top',
+        text:'Países destacados',
+        font:{
+            size:20,
+            family:"Delius",
+        }
+    }
+    }
+  }
+});
+
+
+//Gráfica pie para género:
+
+let arrayForGender= [];
+let count =[];
+for(let element of porcentGender(dataForAthletes,2023) ){
+  arrayForGender.push(element.gender);
+  count.push(element.valor);
+}
+const secondGraphic = new Chart(graphicForGender, {
+    type: "pie",
+    data:{
+        labels:arrayForGender,
+        datasets:[
+            {
+                label:'Género',
+                data: [964,1054],
+                backgroundColor:['rgb(216, 133, 163)','rgb(120, 151, 171)']
+            },
+        ],
+    },
+    options:{
+        maintainAspectRatio:false,
+        plugins: {
+            title:{
+                display:true,
+                align:'center',
+                position:'top',
+                text:`Participantes por género: Mujeres: ${count[0]}%, Hombres:${count[1]}%`,
+                font:{
+                    size:20,
+                    family:"Delius",
+                },
+            },
+            legend: {
+              position: "top",
+            }
+          }
+    },
+}
+)
